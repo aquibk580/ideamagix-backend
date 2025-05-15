a75e68073c410cd6551324734e0f93cbc1721e48
aquibk580 <aquibk580@gmail.com>
2025-05-15 15:47:27 +0530

    updated cookie options

--- src/controllers/patient/auth.ts
+++ src/controllers/patient/auth.ts
@@ -73,8 +73,8 @@ async function signup(req: Request, res: Response): Promise<void> {
 
     res.cookie("patientToken", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -137,8 +137,8 @@ async function signin(req: Request, res: Response): Promise<void> {
 
     res.cookie("patientToken", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -159,8 +159,8 @@ async function signin(req: Request, res: Response): Promise<void> {
 function logout(req: Request, res: Response) {
   res.clearCookie("patientToken", {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Lax",
   });
   res.status(200).json({ message: "Logged out successfully" });
 }
